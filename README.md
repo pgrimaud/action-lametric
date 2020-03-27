ea9ea84ff8fb3fac0c945117104821f03664f79b
Pierre Grimaud <grimaud.pierre@gmail.com>
2020-03-27 16:46:20 +0100

    Update message on LaMetric

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# action-lametric
-Notify your LaMetric using Github Actions
+# Github Actions x LaMetric
+
+![lametric](https://github.com/pgrimaud/action-lametric/workflows/lametric/badge.svg?branch=master)
+
+Send notification to your LaMetric using Github Actions
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ -z "$NOTIFICATION_ICON" ]; then
+  NOTIFICATION_ICON=21802
+fi
+
+if [ -z "$NOTIFICATION_TEXT" ]; then
+  TEXT="CI of ${GITHUB_REPOSITORY}"
+else
+  TEXT=$NOTIFICATION_TEXT
+fi
+
 curl -X POST \
 -H "Accept: application/json" \
 -H "X-Access-Token: ${LAMETRIC_ACCESS_TOKEN}" \
@@ -7,8 +17,8 @@ curl -X POST \
 -d '{
     "frames": [
         {
-            "text": "Actions",
-            "icon": "i21802",
+            "text": "'"${TEXT}"' #'${GITHUB_RUN_NUMBER}'",
+            "icon": "'${NOTIFICATION_ICON}'",
             "index": 0
         }
     ]
